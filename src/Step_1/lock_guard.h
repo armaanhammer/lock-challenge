4c2f672109a0b2471078fb3566d91554fa17711f
Armaan Roshani <armaan@gmail.com>
2018-12-03 17:36:09 -0800

    Update lock_guard.h

--- src/Step_1/lock_guard.h
+++ src/Step_1/lock_guard.h
@@ -1,5 +1,8 @@
 
-
+/** @brief class declaration
+ *
+ * Copied from online resources
+ */
 template <class Mutex>
 class lock_guard {
  public:
@@ -12,3 +15,38 @@ class lock_guard {
  private:
     mutex_type& pm; // exposition only
 };
+
+
+
+
+
+/** @brief A movable scoped lock type, duplicated from std lib
+ *
+ * This class has been kept as identical to std::lock_guard as possible.
+ *
+ * A unique_lock controls mutex ownership within a scope. Ownership of the
+ * mutex can be delayed until after construction and can be transferred
+ * to another unique_lock by move construction or move assignment. If a
+ * mutex lock is owned when the destructor runs ownership will be released.
+ */
+template<typename _Mutex>
+  class lock_guard
+  {
+  public:
+    typedef _Mutex mutex_type;
+
+    explicit lock_guard(mutex_type& __m) : _M_device(__m)
+    { _M_device.lock(); }
+
+    lock_guard(mutex_type& __m, adopt_lock_t) : _M_device(__m)
+    { } // calling thread owns mutex
+
+    ~lock_guard()
+    { _M_device.unlock(); }
+
+    lock_guard(const lock_guard&) = delete;
+    lock_guard& operator=(const lock_guard&) = delete;
+
+  private:
+    mutex_type&  _M_device;
+  };
