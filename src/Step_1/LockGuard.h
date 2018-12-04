74a68c5bfd1cbfdfe3b826dc407080b9e8d35e1d
Armaan Roshani <armaan@gmail.com>
2018-12-04 12:20:35 -0800

    Update LockGuard.h

--- src/Step_1/LockGuard.h
+++ src/Step_1/LockGuard.h
@@ -1,7 +1,7 @@
 #ifndef _LOCK_G_HDR  // header guard
 #define _LOCK_G_HDR
 
-#include <mutex>          // std::mutex, std::adopt_lock
+#include <mutex>     // std::mutex, std::adopt_lock
 
 
 namespace chal { //challenge namespace
@@ -15,20 +15,20 @@ namespace chal { //challenge namespace
      * to another unique_lock by move construction or move assignment. If a
      * mutex lock is owned when the destructor runs ownership will be released.
      */
-    template<typename _Mutex>
+    template<typename _Mutex>  // returns type determined by calling function
     class LockGuard
     {
         public:
             typedef _Mutex mutex_type;
 
-            explicit LockGuard(mutex_type& __m) : _M_device(__m)
+            explicit LockGuard(mutex_type& __m) : _M_device(__m)  //no implicit constructor
                 { _M_device.lock(); }
 
             LockGuard(mutex_type& __m, std::adopt_lock_t) : _M_device(__m)
                 { } // calling thread owns mutex
 
             ~LockGuard()
-            { _M_device.unlock(); }
+                { _M_device.unlock(); }
 
             LockGuard(const LockGuard&) = delete;
             LockGuard& operator=(const LockGuard&) = delete;
@@ -36,7 +36,6 @@ namespace chal { //challenge namespace
         private:
             mutex_type&  _M_device;
     };
-
 }
 
 #endif
