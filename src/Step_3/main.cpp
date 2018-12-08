5e418e9069cec23c7e491c4c2605cb54e410f161
Armaan Roshani <armaan@gmail.com>
2018-12-07 23:52:29 -0800

    lock_guard or unique_lock?
    Trying to figure if lock_guard actually has the functionality I
    need to be able to switch between threads. It may not. If not, I
    will need to use unique_lock.

--- src/Step_3/main.cpp
+++ src/Step_3/main.cpp
@@ -5,11 +5,17 @@
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
 #include <mutex>          // std::mutex, std::adopt_lock
+#include <random>
+
 #include "LockGuard.h"    // chal::LockGuard
+#include <vector>
 
 #define NUM_THDS 30
 
+
+/// Globals
 std::mutex mtx;           // mutex for critical section
+
 
 
 /** \brief thread print function
@@ -38,6 +44,14 @@ void thd_worker (int this_id, int next_id) {
     mtx.lock();
     chal::LockGuard<std::mutex> lck (mtx, std::adopt_lock);
 
+    // sleep
+    std::this_thread::sleep_for(chrono::second(2));
+
+    // debug
+    std::cout << "this: " << this_id 
+              << " next: " << next_id << std::endl;
+
+
     std::string msg = "testing!";
 
     thd_printer(this_id, msg);
@@ -46,11 +60,24 @@ void thd_worker (int this_id, int next_id) {
 
 int main ()
 {
+    //std::vector<std::thread> threads
     std::thread threads[NUM_THDS]; //creates an array of NUM_THDS of thread objects
     
-    // spawn NUM_THDS threads:
-    for (int i=0; i<NUM_THDS; ++i)
-        threads[i] = std::thread(thd_worker,i+1);
+/*
+    for (int th : sizeof(threads))
+        threads[th] = std::thread(thd_worker,th+1,th+2);
+*/
+    int this_id, next_id;
+
+    /// spawn NUM_THDS threads:
+    for (int i=0; i<NUM_THDS; ++i) {
+        this_id = i+1;
+        if (i == NUM_THDS-1) next_id=1;
+        else next_id = i+2;
+        threads[i] = std::thread(thd_worker, this_id, next_id);
+    }
+
+    std::cout << "uh?" << std::endl;
 
     for (auto& th : threads) th.join();
 
