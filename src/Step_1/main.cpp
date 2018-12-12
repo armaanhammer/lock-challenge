82c86f7a7b961a22f21ffaca2cb3f6b68e55931a
Armaan Roshani <armaan@gmail.com>
2018-12-12 15:59:03 -0800

    Update main.cpp

--- src/Step_1/main.cpp
+++ src/Step_1/main.cpp
@@ -1,6 +1,6 @@
 // constructing lock_guard with adopt_lock
-
-//NOTE: needs to be compiled with flag -pthread.
+// NOTE: needs to be compiled with flag -pthread.
+// SOURCE: http://www.cplusplus.com/reference/mutex/mutex/lock/
 
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
@@ -11,6 +11,8 @@ std::mutex mtx;           // mutex for critical section
 
 void print_thread_id (int id) {
     mtx.lock();
+    
+    // Swap next two lines to test chal::LockGuard against std::lock_guard
     //std::lock_guard<std::mutex> lck (mtx, std::adopt_lock);
     chal::LockGuard<std::mutex> lck (mtx, std::adopt_lock);
 
