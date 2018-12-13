371b441a4364d0a87a812e67775eabee0b5846e0
Armaan Roshani <armaan@gmail.com>
2018-12-12 17:51:52 -0800

    fixing Step_2

--- src/Step_2/main.cpp
+++ src/Step_2/main.cpp
@@ -1,6 +1,6 @@
 // constructing lock_guard with adopt_lock
-
-//NOTE: needs to be compiled with flag -pthread.
+// NOTE: needs to be compiled with flag -pthread.
+// SOURCE: http://www.cplusplus.com/reference/mutex/lock_guard/
 
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
@@ -22,8 +22,8 @@ void print_thread_id (int id) {
 int main ()
 {
     std::thread threads[THREAD_NUM]; //creates an array of THREAD_NUM number of thread objects
-    // spawn 10 threads:
-    for (int i=THREAD_NUM; i<10; ++i)
+    // spawn THREAD_NUM threads:
+    for (int i=0; i<THREAD_NUM; ++i)
         threads[i] = std::thread(print_thread_id,i+1);
 
     for (auto& th : threads) th.join();
